b2ebcc23020190df81eab52bd61363d08e5555df
greggman <greggman@0fd63cac-a00d-0410-92bc-bc94234f7ac4>
2006-01-12 01:36:32 +0000

    added MEMFILE_Init started adding support to read layers

--- trunk/elibs/lib/echidna/memfile.c
+++ trunk/elibs/lib/echidna/memfile.c
@@ -17,7 +17,7 @@
    TABS : 5 9
 
    HISTORY
-		03/28/96 : Created.
+        03/28/96 : Created.
 
  *************************************************************************/
 
@@ -47,78 +47,83 @@
 
 /**************************** R O U T I N E S ****************************/
 
+// for creating your own mem file
+void MEMFILE_Init(MEMFILE* mf, void* buf, long len)
+{
+    memset (mf, 0, sizeof (MEMFILE));
+
+    mf->buffer    = (uint8*)buf;
+    mf->curPtr    = mf->buffer;
+    mf->size      = len;
+    mf->bytesLeft = len;
+}
+
 MEMFILE *MEMFILE_Load (const char *filename)
 {
-	MEMFILE* mf = NULL;
-	int		fh;
-	long	len;
-	
-	fh = EIO_ReadOpen(filename); 
-	if (fh != (-1))
-	{
-		len = EIO_FileLength(fh);
-		if (len)
-		{
-			mf = (MEMFILE*)malloc (len + sizeof (MEMFILE));
-			if (mf)
-			{
-				memset (mf, 0, sizeof (MEMFILE));
+    MEMFILE* mf = NULL;
+    int     fh;
+    long    len;
 
-				mf->buffer = ((uint8*)mf)+sizeof (MEMFILE);
-			
-				mf->curPtr    = mf->buffer;
-				mf->size      = len;
-				mf->bytesLeft = len;
-	
-				EIO_Read (fh, mf->buffer, len);
-			}
-		}
-		EIO_Close (fh);
-	}
-	return mf;
+    fh = EIO_ReadOpen(filename);
+    if (fh != (-1))
+    {
+        len = EIO_FileLength(fh);
+        if (len)
+        {
+            mf = (MEMFILE*)malloc (len + sizeof (MEMFILE));
+            if (mf)
+            {
+                MEMFILE_Init(mf, ((uint8*)mf)+sizeof (MEMFILE), len);
+
+                EIO_Read (fh, mf->buffer, len);
+            }
+        }
+        EIO_Close (fh);
+    }
+    return mf;
 }
 
 void MEMFILE_Close (MEMFILE *mf)
 {
-	free (mf);
+    free (mf);
 }
 
 int MEMFILE_Read(MEMFILE *mf, void *buf, long len)
 {
-	if (mf->bytesLeft)
-	{
-		long	copyBytes;
+    if (mf->bytesLeft)
+    {
+        long    copyBytes;
 
-		copyBytes = (mf->bytesLeft < len) ? mf->bytesLeft : len;
+        copyBytes = (mf->bytesLeft < len) ? mf->bytesLeft : len;
 
-		memcpy (buf, mf->curPtr, copyBytes);
+        memcpy (buf, mf->curPtr, copyBytes);
 
-		mf->bytesLeft -= copyBytes;
-		mf->curPtr    += copyBytes;
+        mf->bytesLeft -= copyBytes;
+        mf->curPtr    += copyBytes;
 
-		return copyBytes;
-	}
-	return 0;
+        return copyBytes;
+    }
+    return 0;
 }
 
 int MEMFILE_Seek (MEMFILE *mf, long pos, int type)
 {
-	switch (type)
-	{
-	case SEEK_SET:
-		mf->curPtr    = mf->buffer + pos;
-		mf->bytesLeft = mf->size   - pos;
-		break;
-	case SEEK_CUR:
-		mf->curPtr    += pos;
-		mf->bytesLeft -= pos;
-		break;
-	case SEEK_END:
-		mf->curPtr     = mf->buffer + mf->size + pos;
-		mf->bytesLeft  = -pos;
-		break;
-	}
-	return mf->size - mf->bytesLeft;
+    switch (type)
+    {
+    case SEEK_SET:
+        mf->curPtr    = mf->buffer + pos;
+        mf->bytesLeft = mf->size   - pos;
+        break;
+    case SEEK_CUR:
+        mf->curPtr    += pos;
+        mf->bytesLeft -= pos;
+        break;
+    case SEEK_END:
+        mf->curPtr     = mf->buffer + mf->size + pos;
+        mf->bytesLeft  = -pos;
+        break;
+    }
+    return mf->size - mf->bytesLeft;
 }
 
 
