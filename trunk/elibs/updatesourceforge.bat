a0f531d48ac0cb4a88707176ca64c5b8210fe870
greggman <greggman@0fd63cac-a00d-0410-92bc-bc94234f7ac4>
2008-04-15 19:18:03 +0000

    for updating the home page

---
+++ trunk/elibs/updatesourceforge.bat
@@ -0,0 +1,19 @@
+@echo off
+if not "%PUTTY_PATH%"=="" goto checkname
+echo you must set the PUTTY_PATH environment variable to the path to putty
+goto error
+
+:checkname
+if not "%SOURCEFORGE_NAME"=="" goto update
+echo you must set the SOURCEFORGE_NAME environment variable to the username of your sourceforge account
+goto error
+
+:update
+"%PUTTY_PATH%\pscp.exe" "docs\The Echidna Libraries.htm" %SOURCEFORGE_NAME%@shell.sourceforge.net:/home/groups/e/el/elibs/htdocs/index.html
+
+goto done
+:error
+pause
+
+:done
+
